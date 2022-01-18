a04949bbbfe37ba53683bcbc87902bb1ab784151
DWangKellogg <dande.wang@kellogg.northwestern.edu>
2022-01-18 10:57:58 +0000

    Class practice

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -7,3 +7,8 @@
 -- +---------------+------------+-----------+-----------+
 
 
+SELECT teams.name, players.first_name, players.last_name, stats.hits
+FROM players INNER JOIN stats ON players.id = stats.player_id INNER JOIN teams ON stats.team_id = teams.id
+WHERE teams.year = "2019"
+ORDER BY home_runs DESC
+LIMIT 1;
